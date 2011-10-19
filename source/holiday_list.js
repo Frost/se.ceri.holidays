644b9a402b13176467b1777975862c6c0bd4fe7d
Martin Frost <frost@ceri.se>
2011-10-19 16:10:05 +0200

    Added filtering.

--- source/holiday_list.js
+++ source/holiday_list.js
@@ -4,7 +4,8 @@ enyo.kind({
   published: {
     headerContent: "",
     year: null,
-    holidays: []
+    holidays: [],
+    listItems: []
   },
   components: [
     {kind: "PageHeader", name: "pageHeader", content: "Swedish Holidays"},
@@ -15,27 +16,58 @@ enyo.kind({
           {name: "holidayDate", flex: 1}
         ]}
       ]}
+    ]},
+    {kind: "Toolbar", components: [
+      {kind: "ToolInput", name: "filterInput", hint: "filter...", oninput: "filterChangeTrigger"}
     ]}
   ],
+
+  filterChangeTrigger: function () {
+    console.log("filtertrigger");
+    enyo.job("filterList", enyo.bind(this, "filterList", 400));
+  },
 
   yearChanged: function () {
     this.$.pageHeader.setContent(this.headerContent + " " + this.year);
   },
 
   holidaysChanged: function () {
+    this.listItems = this.holidays;
+
     for (var index in this.holidays) {
       this.loadHolidays(enyo.VirtualRepeater, index);
     }
     this.$.holidayList.render();
   },
 
+  listItemsChanged: function () {
+    for (var index in this.listItems) {
+      this.loadHolidays(enyo.VirtualRepeater, index);
+    }
+    this.$.holidayList.render();
+  },
+
   loadHolidays: function (inSender, inIndex) {
-    var data = this.holidays[inIndex];
+    var data = this.listItems[inIndex];
     var datefmt = new enyo.g11n.DateFmt({"format": "EEEE dd MMMM"});
     if (data) {
       this.$.holidayName.setContent(data.name);
       this.$.holidayDate.setContent(datefmt.format(data.date));
       return true;
     }
+  },
+
+  filterList: function () {
+    var filter = this.$.filterInput.getValue();
+    console.log("filter: " + filter);
+
+    var selectedValues = [];
+    for (index in this.holidays) {
+      var data = this.holidays[index];
+      if (data && (data.name.indexOf(filter) !== -1 )) {
+        selectedValues.push(data);
+      }
+    }
+    this.setListItems(selectedValues);
   }
 });
